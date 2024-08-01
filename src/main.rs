6d87074b484770157b9ce99fc00baa8e20b1990f
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2024-08-01 23:15:13 +0100

    attempt but not working chunk data..

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use bedrock_rs::core::int::VAR;
+use bedrock_rs::nbt::{self, NbtTag};
 use bedrock_rs::proto::connection::Connection;
 use bedrock_rs::proto::gamepacket::GamePacket;
-use bedrock_rs::proto::listener;
 use bedrock_rs::proto::login::login_to_server;
 use bedrock_rs::proto::login::provider::DefaultLoginProvider;
 use bedrock_rs::proto::packets::chunk_radius_updated::ChunkRadiusUpdatedPacket;
+use bedrock_rs::proto::packets::level_chunk::LevelChunkPacket;
+use bedrock_rs::proto::types::chunk_pos::ChunkPos;
 use tokio::main;
+use std::collections::HashMap;
 use std::net::{SocketAddr, SocketAddrV4};
 use std::str::FromStr;
 use std::time::Duration;
+use bedrock_rs::world::palet::PalettedStorage;
 
 const TICK_INTERVAL: Duration = Duration::from_millis(50);
 
@@ -69,6 +73,38 @@ async fn handle_connection(conn: Connection) {
             GamePacket::MovePlayer(packet) => {
                 println!("{:?} {:?} {:?}", packet.position, packet.rotation, packet.head_rotation);
             },
+            GamePacket::SetLocalPlayerAsInitialized(packet) => {
+                let mut palette: Vec<NbtTag> = Vec::new();
+
+                let mut fields: HashMap<String, NbtTag> = HashMap::new();
+                fields.insert(String::from("name"), NbtTag::String(String::from("minecraft:air")));
+
+                let compound = NbtTag::Compound(fields);
+                palette.push(compound);
+
+                let storage = PalettedStorage {
+                    blocks: [0; 4096],
+                    palette
+                };
+
+                // Serialize to chunk data
+                let mut chunk_serialized_data: Vec<u8> = Vec::new();
+                chunk_serialized_data.push(8); // format version 8
+                chunk_serialized_data.push(1); // only 1 storage (further ones are used for water)
+                chunk_serialized_data.extend_from_slice(&storage.encode(true));
+
+                let chunk_packet = LevelChunkPacket {
+                    chunk_position: ChunkPos::new(0, 0),
+                    dimension_id: VAR::<i32>::new(0),
+                    sub_chunk_count: VAR::<u32>::new(1),
+                    cache_enabled: false,
+                    serialized_chunk_data: chunk_serialized_data,
+                    client_needs_to_request_subchunks: false,
+                    client_request_subchunk_limit: VAR::new(0xFFFFFFFFu32 as i32)
+                };
+
+                shard.send(GamePacket::LevelChunk(chunk_packet)).await.unwrap();
+            }
             _ => {
                 println!("unhandled {:?}", game_packet);
             }
