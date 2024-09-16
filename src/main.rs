07e6dac5db72eb09daee7d92f726f10a7e186647
theaddon <theaddonn@gmail.com>
2024-09-16 11:37:02 +0200

    attempt at sending chunks

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use std::collections::HashMap;
 use crate::config::RAstraConfig;
 use bedrockrs::proto::login::login_to_server;
 use bedrockrs::proto::login::provider::DefaultLoginProvider;
 use bedrockrs::proto::{connection, listener};
-use log::{debug, error, info};
+use log::{debug, error, info, warn};
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::process::exit;
 use std::str::FromStr;
 use std::time::Duration;
+use bedrockrs::core::int::{LE, VAR};
+use bedrockrs::proto::gamepackets::GamePackets;
+use bedrockrs::proto::packets::chunk_publisher_update::ChunkPublisherUpdatePacket;
+use bedrockrs::proto::packets::chunk_radius_updated::ChunkRadiusUpdatedPacket;
+use bedrockrs::proto::packets::level_chunk::LevelChunkPacket;
+use bedrockrs::proto::types::block_pos::BlockPos;
+use bedrockrs::proto::types::chunk_pos::ChunkPos;
+use bedrockrs::world::palette::PalettedStorage;
 
 mod config;
 mod logger;
@@ -69,12 +78,76 @@ async fn handle_conn(connection: connection::Connection) {
         .unwrap();
 
     loop {
-        let pk = shard.recv().await;
+        let pk = shard.recv().await.unwrap();
+        match pk {
+            GamePackets::SetLocalPlayerAsInitialized(_) => {
+                tokio::time::sleep(Duration::from_millis(1000)).await;
 
-        info!("{pk:#?}");
+                let mut palette: Vec<nbtx::Value> = Vec::new();
 
-        if pk.is_err() {
-            break;
+                let mut fields: HashMap<String, nbtx::Value> = HashMap::new();
+                fields.insert(String::from("name"), nbtx::Value::String(String::from("minecraft:air")));
+
+                let compound = nbtx::Value::Compound(fields);
+                palette.push(compound);
+
+                let mut fields: HashMap<String, nbtx::Value> = HashMap::new();
+                fields.insert(String::from("name"), nbtx::Value::String(String::from("minecraft:dirt")));
+
+                let compound = nbtx::Value::Compound(fields);
+                palette.push(compound);
+
+                let mut blocks = [0; 4096];
+
+                for i in 0..16*16 {
+                    blocks[i] = 1;
+                }
+
+                let storage = PalettedStorage {
+                    blocks,
+                    palette
+                };
+
+                // Serialize to chunk data
+                let mut chunk_serialized_data: Vec<u8> = Vec::new();
+                chunk_serialized_data.push(8); // format version 8
+                chunk_serialized_data.push(1); // only 1 storage (further ones are used for water)
+                chunk_serialized_data.extend_from_slice(&storage.encode(true));
+
+                for x in -8..8 {
+                    for y in -8..8 {
+                        shard.send(GamePackets::LevelChunk(LevelChunkPacket {
+                            chunk_position: ChunkPos::new(x, y),
+                            dimension_id: VAR::new(0),
+                            sub_chunk_count: VAR::new(1),
+                            cache_enabled: false,
+                            serialized_chunk_data: chunk_serialized_data.clone(),
+                            client_needs_to_request_subchunks: false,
+                            client_request_subchunk_limit: VAR::new(0xFFFFFFFFu32 as i32),
+                        })).await.unwrap();
+                    }
+                }
+
+                shard.send(GamePackets::ChunkPublisherUpdate(ChunkPublisherUpdatePacket{
+                    position: BlockPos::new(0, 0, 0),
+                    radius: VAR::new(16*16),
+                    saved_chunks: vec![]
+                })).await.unwrap();
+            }
+            GamePackets::PacketViolationWarning(pk) => {
+                warn!("======= PacketViolationWARNING =======");
+                warn!("PK ID:    {}", pk.violating_packet_id.into_inner());
+                warn!("SEVERITY: {}", pk.severity.into_inner());
+                warn!("KIND:     {}", pk.kind.into_inner());
+                warn!("CONTEXT:  {}", pk.context);
+            }
+            GamePackets::RequestChunkRadius(pk) => {
+                shard.send(GamePackets::ChunkRadiusUpdate(ChunkRadiusUpdatedPacket{
+                    chunk_radius: VAR::new(pk.chunk_radius_max as i32),
+                })).await.unwrap();
+                shard.flush().await.unwrap();
+            }
+            _ => {}
         }
     }
 }
